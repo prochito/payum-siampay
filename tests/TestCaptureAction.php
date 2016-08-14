42a29caaa5310406396a87e6cf558efcfe0a7ab3
K.M. Tahsin Hassan Rahit <tahsin.rahit@gmail.com>
2016-08-14 16:06:21 +0600

    first commit

---
+++ tests/TestCaptureAction.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rahit
+ * Date: 8/11/16
+ * Time: 11:12 PM
+ */
+class TestCaptureAction extends Payum\Core\Tests\Action\CapturePaymentActionTest{
+
+
+    public function testExecute() {
+        //$model = Payum\Core\Model\Payment::
+    }
+}
